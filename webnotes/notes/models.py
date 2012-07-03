47d59fc870caaa81fd7f28704e1a917851d243ae
AITI Student <aiti-student@AITI-83.(none)>
2012-07-03 15:47:08 +0000

    like rocket  science

---
+++ webnotes/notes/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+class Notes(models.Model):
+    title = models.CharField(max_length=255)
+    author = models.CharField(max_length=255)
+    content = models.TextField()
+    def __unicode__(self):
+	return self.title
+    
+#    author = models.CharField(max_length=255)
+#    content = models.TextField()
+#    def __unicode__(self):
+#	return self.author
+
+# Create your models here.
+
+
